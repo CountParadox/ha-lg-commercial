395b3989a07dff33aa831801226c8fb5fe0400c1
Lewys <countparadox@gmail.com>
2026-02-27 20:01:48 +1100

    Add constants file

---
+++ custom_components/lg_commercial/const.py
@@ -0,0 +1,17 @@
+DOMAIN = "lg_commercial"
+
+DEFAULT_PORT = 9761
+DEFAULT_SCAN_INTERVAL = 20
+
+CONF_USE_ALTERNATE = "use_alternate"
+CONF_SET_ID = "set_id"
+
+LG_DEFAULT_SET_ID = "01"
+
+# Known LG MAC prefixes for discovery
+LG_OUIS = [
+    "58:FD:B1",
+    "00:1E:75",
+    "A0:39:F7",
+    "64:BC:0C"
+]
