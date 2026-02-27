112ea5d7ff384d327340ae820b5008bdd920365f
Lewys <countparadox@gmail.com>
2026-02-27 20:02:08 +1100

    Add init and multi-device setup

---
+++ custom_components/lg_commercial/__init__.py
@@ -0,0 +1,48 @@
+import logging
+from homeassistant.config_entries import ConfigEntry
+from homeassistant.core import HomeAssistant
+from homeassistant.helpers.typing import ConfigType
+from homeassistant.const import Platform
+
+from .const import DOMAIN
+from .coordinator import LGCoordinator, LGDisplayAPI
+
+_LOGGER = logging.getLogger(__name__)
+
+PLATFORMS = [Platform.MEDIA_PLAYER]
+
+
+async def async_setup(hass: HomeAssistant, config: ConfigType) -> bool:
+    hass.data.setdefault(DOMAIN, {})
+    return True
+
+
+async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
+    hass.data.setdefault(DOMAIN, {})
+
+    api = LGDisplayAPI(
+        host=entry.data["ip_address"],
+        port=entry.data["port"],
+        mac=entry.data["mac"],
+        use_alternate=entry.data.get("use_alternate", False),
+        set_id=entry.data.get("set_id", "01"),
+    )
+
+    coordinator = LGCoordinator(hass, api)
+    await coordinator.async_config_entry_first_refresh()
+
+    hass.data[DOMAIN][entry.entry_id] = coordinator
+
+    await hass.config_entries.async_forward_entry_setups(entry, PLATFORMS)
+
+    return True
+
+
+async def async_unload_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
+    unload_ok = await hass.config_entries.async_unload_platforms(
+        entry, PLATFORMS
+    )
+    if unload_ok:
+        hass.data[DOMAIN].pop(entry.entry_id)
+
+    return unload_ok
