27eebdd8c019363b991d51db06f5fde4b6dbac66
Lewys Martin <countparadox@gmail.com>
2026-02-28 12:28:09 +1100

    Fix config flow compatibility for input selection

--- custom_components/lg_commercial/config_flow.py
+++ custom_components/lg_commercial/config_flow.py
@@ -1,7 +1,7 @@
 import voluptuous as vol
 from homeassistant import config_entries
 from homeassistant.const import CONF_NAME, CONF_IP_ADDRESS, CONF_PORT
-from homeassistant.helpers import selector
+from homeassistant.helpers import config_validation as cv, selector
 
 from .const import (
     AVAILABLE_INPUTS,
@@ -19,7 +19,20 @@ class ConfigFlow(config_entries.ConfigFlow, domain=DOMAIN):
     VERSION = 1
 
     async def async_step_user(self, user_input=None):
+        errors = {}
+
         if user_input is not None:
+            set_id = str(user_input.get(CONF_SET_ID, LG_DEFAULT_SET_ID)).strip()
+            if not set_id.isdigit() or len(set_id) > 2:
+                errors[CONF_SET_ID] = "invalid_set_id"
+            else:
+                user_input[CONF_SET_ID] = f"{int(set_id):02d}"
+
+            enabled_inputs = user_input.get(CONF_ENABLED_INPUTS, [])
+            if not enabled_inputs:
+                errors[CONF_ENABLED_INPUTS] = "select_at_least_one"
+
+        if user_input is not None and not errors:
             return self.async_create_entry(
                 title=user_input[CONF_NAME],
                 data=user_input,
@@ -34,13 +47,11 @@ class ConfigFlow(config_entries.ConfigFlow, domain=DOMAIN):
                 vol.Optional(CONF_WOL_ENTITY): selector.EntitySelector(
                     selector.EntitySelectorConfig(domain=["switch", "button"])
                 ),
-                vol.Optional(CONF_SET_ID, default=LG_DEFAULT_SET_ID): vol.Match(r"^\d{2}$"),
+                vol.Optional(CONF_SET_ID, default=LG_DEFAULT_SET_ID): cv.string,
                 vol.Optional(CONF_USE_ALTERNATE, default=False): bool,
-                vol.Optional(CONF_ENABLED_INPUTS, default=list(AVAILABLE_INPUTS.keys())): selector.SelectSelector(
-                    selector.SelectSelectorConfig(
-                        options=list(AVAILABLE_INPUTS.keys()),
-                        multiple=True,
-                    )
+                vol.Optional(CONF_ENABLED_INPUTS, default=list(AVAILABLE_INPUTS.keys())): cv.multi_select(
+                    AVAILABLE_INPUTS
                 ),
             }),
+            errors=errors,
         )
